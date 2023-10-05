9c768e911b67b4f1bb9fc0d4f24bb1687e48bcb5
Matheus Feitosa <matheusfeitosa1277@gmail.com>
2023-10-05 16:19:33 -0300

    add: gaussian_elimination_var

--- src/elimination.c
+++ src/elimination.c
@@ -8,8 +8,7 @@ void pivot(Matrix A, Vector b, int order, int p) {
     IR v_aux = b[p];
 
     for (int i = p; i < order; ++i) // Encontra o maior coeficiente da coluna p //
-        //if (fabs(A[i][p].b) - fabs(A[max][p].b) > DBL_EPSILON) // !!! //
-        if (fabs(A[i][p].a) > fabs(A[max][p].a))  // !!! //
+        if (fabs(A[i][p].a) > fabs(A[max][p].a))
             max = i;
 
     A[p] = A[max];
@@ -31,8 +30,6 @@ void gaussian_elimination(Matrix A, Vector x, Vector b, int order) {
         for (k = i+1; k < order; ++k) {   /* Linhas */
             ir_div(&m, A[k][i], A[i][i]);
 
-            vector_print_inf(&m, 1);
-
             for (j = i; j < order; ++j) { /* Deslocamento A[k][j] */
                 ir_mul(&aux, A[i][j], m);
                 ir_sub(&A[k][j], A[k][j], aux);
@@ -44,11 +41,40 @@ void gaussian_elimination(Matrix A, Vector x, Vector b, int order) {
 
     }
 
-    matrix_print_inf(A, order, order);
-    matrix_print_sup(A, order, order);
+    back_substitution(A, x, b, order);
+}
+
+/* 2. Forma classica com pivoteamento, sem o calculo dos multiplicadores */
+
+void gaussian_elimination_var(Matrix A, Vector x, Vector b, int order) {
+    int i, j, k;
+    IR m, aux0, aux1;
+
+
+    for (i = 0; i < order; ++i) {       /* Colunas */
+        //pivot(A, b, order, i);
+
+        for (k = i+1; k < order; ++k) { /* Linhas */
+            m = A[k][i];
+
+            for (j = i; j < order; ++j) { /* Deslocamento A[k][j] */
+                ir_mul(&aux0, A[k][j], A[i][i]);
+                ir_mul(&aux1, A[i][j], m);
+
+                ir_sub(&A[k][j], aux0, aux1);
+            }
+
+            ir_mul(&aux0, b[k], A[i][i]);
+            ir_mul(&aux1, b[i], m);
+
+            ir_sub(&b[k], aux0, aux1);
+
+        }
+    }
 
     back_substitution(A, x, b, order);
 }
+
 
 /* A is upper triangular */
 
