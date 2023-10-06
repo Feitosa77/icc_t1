6995e8078ff617a49e1f4b2ebe0edb39ef8f11e6
Edison Matias <eluizmatiasjunior@gmail.com>
2023-10-06 16:57:35 -0300

    update: finished likwid script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+INPUT="teste.in"
 BIN="./bin/ajustePol"
-INPUT="pontos.in"
 
-likwid-perfctr -C 3 -g "FLOPS_DP" -m $BIN < $INPUT | grep "MFLOP/s"
-likwid-perfctr -C 3 -g "FLOPS_AVX" -m $BIN < $INPUT | grep "MFLOP/s"
-likwid-perfctr -C 3 -g "ENERGY" -m $BIN < $INPUT | grep "Energy"
+likwid-perfctr -C 3 -g FLOPS_DP -m $BIN < $INPUT | sed -n -e 6,9p
+likwid-perfctr -C 3 -g FLOPS_DP -m $BIN < $INPUT | grep "MFLOP/s"
+likwid-perfctr -C 3 -g ENERGY -m $BIN < $INPUT | grep "Energy \[J\]"
