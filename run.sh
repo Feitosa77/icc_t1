e417228bf2b68208d3ac3dbfc19d40c7fb622a73
Edison Matias <eluizmatiasjunior@gmail.com>
2023-10-06 14:39:04 -0300

    update (likwid): added FLOPS_DP, FLOPS_AVX & ENERGY groups

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 BIN="./bin/ajustePol"
-
 INPUT="pontos.in"
-OUTPUT="resultado.out"
 
-likwid-perfctr -C 3 -g FLOPS_DP -m $BIN < $INPUT > $OUTPUT # | grep "DP MFLOP/s" | awk '{print $5}' | grep -v "|"
-
-echo "Relatório gerado em $OUTPUT"
+likwid-perfctr -C 3 -g "FLOPS_DP" -m $BIN < $INPUT | grep "MFLOP/s"
+likwid-perfctr -C 3 -g "FLOPS_AVX" -m $BIN < $INPUT | grep "MFLOP/s"
+likwid-perfctr -C 3 -g "ENERGY" -m $BIN < $INPUT | grep "Energy"
