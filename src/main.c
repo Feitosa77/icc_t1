f2034038110df70f1ac4e52683c85939152f75dd
Matheus Feitosa <matheusfeitosa1277@gmail.com>
2023-10-06 17:17:37 -0300

    add: calculo residuo

--- src/main.c
+++ src/main.c
@@ -19,8 +19,25 @@ int main()
     vector_print(coefficients, order);
     printf("%lf\n%lf\n", time.generating, time.solving);
 
-    vector_destroy(coefficients);
+    IR r, aux;
+    
+    for (int i = 0; i < length; ++i) {
+        r.a = 0.0; r.b = 0.0;
+
+        for (int j = 0; j < order; ++j) { // Calculo y_i
+            ir_ipow(&aux, points[i]->x, j);
+            ir_mul(&aux, coefficients[j], aux);
+            ir_sum(&r, r, aux);
+        }
+
+        ir_sub(&r, points[i]->y, r);
+
+        vector_print(&r, 1);
+    }
+
+
     free_points(points, length);
+    vector_destroy(coefficients);
 
     return EXIT_SUCCESS;
 }
